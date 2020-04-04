dfc34d6e795aa488771e9c1b2375b92970e1df05
purva <purvajg@gmail.com>
2020-04-03 18:32:10 -0700

    Updated firebase function to sync contacts between phone book and app database

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,53 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-var serviceAccount = require("/Users/rohandalvi/Downloads/gupshop-27dcc-firebase-adminsdk-cxpq6-32df356e81.json");
+var serviceAccount = require("/Users/purvadalvi/Downloads/gupshop-27dcc-firebase-adminsdk-cxpq6-5e458a4851.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://gupshop-27dcc.firebaseio.com"
 });
 
+var  arrayOfContactNumbers = [];
+var arrayOfUnionContacts = [];
+var db = admin.firestore();
 exports.addMessage = functions.https.onRequest(async (req,res) => {
   const original = req.query.userPhoneNumber;
   const stringOfContactNumebrs = req.query.contactNumbers;
-  const arrayOfContactNumbers = stringOfContactNumebrs.split(",");
-  console.log("original ",req.query);
-  const snapshot = await admin.database().ref('/friends_'+original).push({contactNumbers:arrayOfContactNumbers});
-  res.redirect(303, snapshot.ref.toString());
+  arrayOfContactNumbers = stringOfContactNumebrs.split(",");
+  var listOfPromises = myFunction(arrayOfContactNumbers);
+  Promise.all(listOfPromises).then(function(snapshots) {
+    console.log("Finally ",arrayOfUnionContacts);
+
+    const snapshot = admin.database().
+    ref('/friends_'+original).
+    set({contactNumbers:arrayOfUnionContacts});
+  })
+  .catch(error => {
+    console.log("Caught error ",error);
+  });
+
 });
+
+
+function myFunction(arrayOfContactNumbers){
+  var listOfPromises = [];
+  arrayOfContactNumbers.forEach(number => {
+    var promise  = getPromise(number);
+    console.log("Found promise ", promise);
+    listOfPromises.push(promise);
+  });
+  return listOfPromises;
+}
+
+function getPromise(number) {
+  return new Promise((resolve, reject)=> {
+    db.collection("users").doc(number).get().then(snapshot => {
+      console.log("snap "+snapshot.id);
+      if(snapshot.data() !=null && snapshot.data().Name !=null) {
+        console.log("Found name "+snapshot.data().Name);
+        arrayOfUnionContacts.push({id: snapshot.id, data: snapshot.data()});
+      }
+      resolve(snapshot);
+    });
+  });
+
+}

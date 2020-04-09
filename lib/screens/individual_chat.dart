83d70d1c3deb707359d148ad276cbb9712fe99a9
purva <purvajg@gmail.com>
2020-04-09 11:11:20 -0700

    Added scrollToTheBottomScreen to the individual_chat

--- lib/screens/individual_chat.dart
+++ lib/screens/individual_chat.dart
@@ -25,6 +25,8 @@ class IndividualChat extends StatefulWidget {
       friendName: friendName);
 }
 
+
+
 class _IndividualChatState extends State<IndividualChat> {
   final String conversationId;
   final String userPhoneNo;
@@ -48,95 +50,100 @@ class _IndividualChatState extends State<IndividualChat> {
     CollectionReference collectionReference =
         Firestore.instance.collection("conversations").document(conversationId).collection("messages");
 
-    return Material(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Material(
-            color: Theme.of(context).primaryColor,
-            child: ListTile(
-              contentPadding: EdgeInsets.symmetric(),
-              leading: CircleAvatar(),
-              title: Text(
-                friendName,//name of the person with whom we are chatting right now, displayed at the top in the app bar
-                style: TextStyle(
-                  color: Colors.white,
+    return Stack(
+      children: <Widget>[
+        Material(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Material(
+                color: Theme.of(context).primaryColor,
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(),
+                  leading: CircleAvatar(),
+                  title: Text(
+                    friendName,//name of the person with whom we are chatting right now, displayed at the top in the app bar
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Put last seen here',
+                  ),
+                  trailing: Wrap(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.video_call),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.phone),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              subtitle: Text(
-                'Put last seen here',
-              ),
-              trailing: Wrap(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.video_call),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.phone),
-                  ),
-                ],
-              ),
             ),
-          ),
-        ),
-        body: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(), //to take out the keyboard when tapped on chat screen
-            child: Flex(
-              direction: Axis.vertical,
-              children: <Widget>[
-                Expanded(
-                  child: StreamBuilder<QuerySnapshot>(
-                      stream: collectionReference.orderBy("timeStamp", descending: true).limit(30).snapshots(),
-                      builder: (context, snapshot) {
+            body: GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(), //to take out the keyboard when tapped on chat screen
+                child: Flex(
+                  direction: Axis.vertical,
+                  children: <Widget>[
+                    Expanded(
+                      child: StreamBuilder<QuerySnapshot>(
+                          stream: collectionReference.orderBy("timeStamp", descending: true).limit(30).snapshots(),
+                          builder: (context, snapshot) {
 
-                        return ListView.separated(
-                          controller:
-                              listScrollController, //for scrolling messages
-                          //shrinkWrap: true,
-                          reverse: true,
-                          itemCount: snapshot.data.documents.length,
-                          itemBuilder: (context, index) {
-                            var messageBody =
-                                snapshot.data.documents[index].data["body"];
-                            var fromName =
-                                snapshot.data.documents[index].data["fromName"];
-                            Timestamp timeStamp =
-                                snapshot.data.documents[index].data["timeStamp"];
-                            bool isMe = false;
+                            return ListView.separated(
+                              controller: listScrollController, //for scrolling messages
+                              //shrinkWrap: true,
+                              reverse: true,
+                              itemCount: snapshot.data.documents.length,
+                              itemBuilder: (context, index) {
+                                var messageBody =
+                                    snapshot.data.documents[index].data["body"];
+                                var fromName =
+                                    snapshot.data.documents[index].data["fromName"];
+                                Timestamp timeStamp =
+                                    snapshot.data.documents[index].data["timeStamp"];
+                                bool isMe = false;
 
-                            if (fromName == userName) isMe = true;
+                                if (fromName == userName) isMe = true;
 
-                            return ListTile(
-                              title: Container(
-                                //fromName:userName? use following widget ToDo
-                                margin: isMe ? EdgeInsets.only(left: 40.0) : EdgeInsets.only(left: 0),
-                                padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0), //for the box covering the text
-                                color: isMe ? Color(0xFFF9FBE7) : Color(0xFFFFEFEE),
-                                child: Text(messageBody,),//message
-                              ),
-                              subtitle: Container(
-                                margin: isMe ? EdgeInsets.only(left: 40.0) : EdgeInsets.only(left: 0),//if not this then the timeStamp gets locked to the left side of the screen. So same logic as the messages above
-                                padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 8.0),//pretty padding- for some margin from the side of the screen as well as the top of parent message
-                                child: Text(//time
-                                  DateFormat("dd MMM kk:mm")
-                                      .format(DateTime.fromMillisecondsSinceEpoch(int.parse(timeStamp.millisecondsSinceEpoch.toString()))),//converting firebase timestamp to pretty print
-                                  style: TextStyle(
-                                      color: Colors.grey, fontSize: 12.0, fontStyle: FontStyle.italic
+                                return ListTile(
+                                  title: Container(
+                                    //fromName:userName? use following widget ToDo
+                                    margin: isMe ? EdgeInsets.only(left: 40.0) : EdgeInsets.only(left: 0),
+                                    padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0), //for the box covering the text
+                                    color: isMe ? Color(0xFFF9FBE7) : Color(0xFFFFEFEE),
+                                    child: Text(messageBody,),//message
                                   ),
-                                ),
+                                  subtitle: Container(
+                                    margin: isMe ? EdgeInsets.only(left: 40.0) : EdgeInsets.only(left: 0),//if not this then the timeStamp gets locked to the left side of the screen. So same logic as the messages above
+                                    padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 8.0),//pretty padding- for some margin from the side of the screen as well as the top of parent message
+                                    child: Text(//time
+                                      DateFormat("dd MMM kk:mm")
+                                          .format(DateTime.fromMillisecondsSinceEpoch(int.parse(timeStamp.millisecondsSinceEpoch.toString()))),//converting firebase timestamp to pretty print
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 12.0, fontStyle: FontStyle.italic
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                              separatorBuilder: (context, index) => Divider(
+                                color: Colors.white,
                               ),
                             );
-                          },
-                          separatorBuilder: (context, index) => Divider(
-                            color: Colors.white,
-                          ),
-                        );
-                      }),
+                          }),
+                    ),
+                    //_scrollToBottomButton(),
+                    _buildMessageComposer(),//write and send new message bar
+                  ],
                 ),
-                _buildMessageComposer(),//write and send new message bar
-              ],
-            ),
-        ), //body
-      ),
+            ), //body
+          ),
+        ),
+        _scrollToBottomButton(),
+      ],
     );
   }
 
@@ -155,7 +162,7 @@ class _IndividualChatState extends State<IndividualChat> {
             this.value=value;
             //_controller.clear();
           },
-          //scrollController: listScrollController,
+          scrollController: listScrollController,
           controller: _controller,//used to clear text when user hits send button
         ),
         trailing: IconButton(
@@ -174,4 +181,24 @@ class _IndividualChatState extends State<IndividualChat> {
       ),
     );
   }
+
+  _scrollToBottomButton(){
+    return Align(
+      alignment: Alignment.centerRight,
+      child: FloatingActionButton(
+        child: IconButton(
+          icon: Icon(Icons.keyboard_arrow_down),
+        ),
+        onPressed: (){
+          listScrollController.animateTo(//for scrolling to the bottom of the screen when a next text is send
+            0.0,
+            curve: Curves.easeOut,
+            duration: const Duration(milliseconds: 300),
+          );
+        },
+      ),
+    );
+  }
 }
+
+

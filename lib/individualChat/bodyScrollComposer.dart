9a109ff9655a4e44ea650a644e213017206805a8
purva <purvajg@gmail.com>
2020-07-22 17:19:36 -0700

    Added pick image from gallery and return user to individual chat screen after picking

--- lib/individualChat/bodyScrollComposer.dart
+++ lib/individualChat/bodyScrollComposer.dart
@@ -291,6 +291,11 @@ class _BodyScrollComposerState extends State<BodyScrollComposer> {
   }
 
   sendImage() async{
+    /// to make the user go to individualChat screen with no bottom bar open
+    /// we have to make sure that Navigator.pop(context); is used so that
+    /// when the user clicks pick image from camera(or any other option),
+    /// he is returned to individualChat with no bottom bar open
+    Navigator.pop(context);
     int numberOfImageInConversation= 0;
     numberOfImageInConversation++;
 
@@ -302,6 +307,11 @@ class _BodyScrollComposerState extends State<BodyScrollComposer> {
   }
 
   sendVideo() async{
+    /// to make the user go to individualChat screen with no bottom bar open
+    /// we have to make sure that Navigator.pop(context); is used so that
+    /// when the user clicks pick image from camera(or any other option),
+    /// he is returned to individualChat with no bottom bar open
+    Navigator.pop(context);
     int numberOfImageInConversation= 0;
     numberOfImageInConversation++;
 

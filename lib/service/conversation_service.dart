ed84b58b36bf2227fec0ee1bbabb9d33a4b3e228
Rohan Dalvi <rohan_dalvi@icloud.com>
2020-08-08 19:59:00 -0700

    Expose api to fix messaging latency issues

--- lib/service/conversation_service.dart
+++ lib/service/conversation_service.dart
@@ -1,30 +1,78 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:gupshop/service/message_service.dart';
 
 class ConversationService{
 
   MessageService messageService = new MessageService();
-  List<Future> expandConversation(String conversationId) {
+  String conversationId;
+  DocumentSnapshot startBefore = null;
+  DocumentSnapshot startAfter = null;
+  List<dynamic> list = [];
 
-    var completer = new Completer();
-    Firestore.instance.collection("conversations").document(conversationId).get().then((documentSnapshot) async {
+  StreamController<QuerySnapshot> streamController = new StreamController();
 
-      var that = this;
-      print("Document Snapshot ${documentSnapshot.data["messageId"]}");
-      //messageService.getMessages(messageIds)
-      var messageIds = documentSnapshot.data["messageId"];
-      await Future.wait(messageService.getMessages(messageIds)).then((value){
+  ConversationService({@required this.conversationId});
 
-        completer.complete(that);
-      });
+  StreamSubscription<QuerySnapshot> subscription;
 
-
-
-
-
-    });
+  void setStartAfter(DocumentSnapshot startAfter) {
+    this.startAfter = startAfter;
 
   }
+
+  subscribeToLatest() {
+
+    if(startAfter == null) {
+      subscription = Firestore.instance.collection("conversations").document(conversationId).collection("messages").limit(3).orderBy("timeStamp", descending: true).snapshots().listen((event) {
+        act(event);
+      });
+    } else {
+      subscription = Firestore.instance.collection("conversations").document(conversationId).collection("messages").orderBy("timeStamp", descending: false).startAfterDocument(startAfter).snapshots().listen((event) {
+        act(event);
+      });
+    }
+
+  }
+
+  void disableActiveSubscription() {
+    subscription.cancel();
+  }
+
+  void act(QuerySnapshot event) {
+    if(event.documents.isNotEmpty) {
+      if(startBefore == null) startBefore = event.documents[event.documents.length-1];
+      print("Got event ${event.documents[0].data["body"]}");
+      startAfter = event.documents[event.documents.length-1];
+      print("StartAfter ${startAfter.documentID}");
+      streamController.add(event);
+      change();
+    }
+  }
+
+  change() {
+    subscription.cancel();
+    subscribeToLatest();
+  }
+
+  Stream<QuerySnapshot> getStream() {
+    return streamController.stream;
+  }
+
+
+  void paginate() {
+    Firestore.instance.collection("conversations").document(conversationId).collection("messages").startAfterDocument(startBefore).orderBy("timeStamp", descending: true).limit(3).snapshots().listen((event) {
+      if(event.documents.isNotEmpty) {
+        print("Start before ${startBefore.data["body"]}");
+        print("Ev ${event.documents.map((e) => e.data["body"])}");
+        startBefore = event.documents[event.documents.length - 1];
+        streamController.add(event);
+      }
+    });
+  }
+
+
+
 }

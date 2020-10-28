7009e5b98a798b4902ea7bd1f2155997ab47da80
purva <purvajg@gmail.com>
2020-10-27 18:17:33 -0700

    Deleted some unwanted imports from bazaarWelcomeRoute

--- lib/navigators/bazaarWelcomeRoute.dart
+++ lib/navigators/bazaarWelcomeRoute.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:gupshop/bazaarOnBoarding/bazaarSubCategorySearch.dart';
 import 'package:gupshop/bazaarOnBoarding/bazaarWelcome.dart';
-import 'package:gupshop/responsive/textConfig.dart';
 
 class BazaarWelcomeRoute{
 

8ca305f5e2c591b9ba5e1ea79283b551e26d1b9a
Rohan <rohan_dalvi@icloud.com>
2020-10-29 17:01:16 -0700

    Added NotificationConsumerMethods to ChangeName.dart(not tested)

--- lib/bazaarProductDetails/changeName.dart
+++ lib/bazaarProductDetails/changeName.dart
@@ -1,21 +1,61 @@
 import 'package:flutter/cupertino.dart';
+import 'package:gupshop/modules/userDetails.dart';
+import 'package:gupshop/notifications/IRules.dart';
+import 'package:gupshop/notifications/NotificationEventType.dart';
+import 'package:gupshop/notifications/application/notificationConsumerMethods.dart';
 import 'package:gupshop/responsive/iconConfig.dart';
 import 'package:gupshop/responsive/imageConfig.dart';
 import 'package:gupshop/responsive/textConfig.dart';
 import 'package:gupshop/widgets/WelcomeScreenWithTextField.dart';
 import 'package:gupshop/widgets/customFlushBar.dart';
 
-class ChangeName extends StatefulWidget {
+class ChangeName extends StatefulWidget implements IRules{
   String businessName;
 
   ChangeName({this.businessName});
 
   @override
   _ChangeNameState createState() => _ChangeNameState();
+
+  @override
+  bool apply(NotificationEventType notificationEventType, String conversationId) {
+    return true;
+  }
 }
 
 class _ChangeNameState extends State<ChangeName> {
   String tempBusinessName;
+
+  /// navigator methods:
+  notificationInit(){
+    NotificationConsumerMethods().notificationInit(
+        runtimeType: this.widget.runtimeType,
+        widget: this.widget,
+        onSelectNotificationFromConsumer: onSelectNotification
+    );
+  }
+
+
+  /// when the user taps the notification:
+  Future<void> onSelectNotification(String payload) async{
+    String userName = await UserDetails().getUserNameFuture();
+    String userNumber = await UserDetails().getUserPhoneNoFuture();
+
+    NotificationConsumerMethods(
+        userName: userName,
+        userPhoneNo: userNumber,
+        customContext: context
+    ).onSelectNotification(payload);
+  }
+
+
+  @override
+  void initState() {
+    notificationInit();
+
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
